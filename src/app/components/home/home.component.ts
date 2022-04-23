de0d2167d004658da7c04d140dea1e72418529c7
Simone Crisafi <simone.crisafi@nttdata.com>
2022-04-23 13:00:50 +0200

    add firebase & firestore to project

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {db} from "../../../main";
+import { addDoc, collection, getDocs } from "firebase/firestore";
 
 @Component({
   selector: 'app-home',
@@ -10,6 +12,34 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    // this.addUserTest();
+    this.getUsersTest();
+
+  }
+
+  public async addUserTest(): Promise<void> {
+    try {
+      const docRef = await addDoc(collection(db, "users"), {
+        first: "Alan",
+        middle: "Mathison",
+        last: "Turing",
+        born: 1912
+      });
+
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  }
+
+  public async getUsersTest() {
+
+    const querySnapshot = await getDocs(collection(db, "users"));
+    querySnapshot.forEach((doc) => {
+      console.log('user -> ' + JSON.stringify(doc));
+    });
+
   }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
 if (environment.production) {
   enableProdMode();
@@ -10,3 +14,22 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyArey947zHHmtMAvwMaa4A6lP6bHlcgRJc",
+  authDomain: "blogsiteproject-570a4.firebaseapp.com",
+  projectId: "blogsiteproject-570a4",
+  storageBucket: "blogsiteproject-570a4.appspot.com",
+  messagingSenderId: "336989683463",
+  appId: "1:336989683463:web:05fdd95c06d65a5c144fa2",
+  measurementId: "G-4T5RYTG7RF"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
