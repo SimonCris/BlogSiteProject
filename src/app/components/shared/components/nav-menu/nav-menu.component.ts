802781415547971c5193b57932349a7cc32cadc7
Simone Crisafi <simone.crisafi@nttdata.com>
2022-04-28 00:21:15 +0200

    update navbar graphics

--- src/app/components/shared/components/nav-menu/nav-menu.component.ts
+++ src/app/components/shared/components/nav-menu/nav-menu.component.ts
@@ -4,7 +4,7 @@ declare var $: any;
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
-  styleUrls: ['./nav-menu.component.scss']
+  styleUrls: ['./nav-menu.component.css']
 })
 export class NavMenuComponent implements OnInit {
 
@@ -14,6 +14,36 @@ export class NavMenuComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    $(document).ready(function () {
+      var trigger = $('.hamburger'),
+        overlay = $('.overlay'),
+        isClosed = false;
+
+      trigger.click(function () {
+        hamburger_cross();
+      });
+
+      function hamburger_cross() {
+
+        if (isClosed == true) {
+          overlay.hide();
+          trigger.removeClass('is-open');
+          trigger.addClass('is-closed');
+          isClosed = false;
+        } else {
+          overlay.show();
+          trigger.removeClass('is-closed');
+          trigger.addClass('is-open');
+          isClosed = true;
+        }
+      }
+
+      $('[data-toggle="offcanvas"]').click(function () {
+        $('#wrapper').toggleClass('toggled');
+      });
+    });
+
   }
 
   /** Metodo che gestisce l'apertura e la chiusura del sidebar */
