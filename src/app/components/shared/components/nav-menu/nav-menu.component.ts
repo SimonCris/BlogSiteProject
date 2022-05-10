9009e36267e4155a94af40acf1b5b099a91c6f71
Simone Crisafi <simone.crisafi@nttdata.com>
2022-05-10 12:29:22 +0200

    add scroll to top when menu section change

--- src/app/components/shared/components/nav-menu/nav-menu.component.ts
+++ src/app/components/shared/components/nav-menu/nav-menu.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {AppRoutingConstants} from "../../../../app-routingConstants";
 import {WorkflowRoutingService} from "../../services/workflow-routing.service";
+import {ScrollToTopComponent} from "../scroll-to-top/scroll-to-top.component";
+import {ViewportScroller} from "@angular/common";
 declare var $: any;
 
 @Component({
@@ -10,10 +12,14 @@ declare var $: any;
 })
 export class NavMenuComponent implements OnInit {
 
-  /** Identifica se il sidebar è aperto o chiuso */
-  isSidebarOpen = false;
+  scrollToTopComponent: ScrollToTopComponent;
 
-  constructor(private workflowRoutingService: WorkflowRoutingService) { }
+  constructor(private workflowRoutingService: WorkflowRoutingService,
+              private scroll: ViewportScroller) {
+
+    this.scrollToTopComponent = new ScrollToTopComponent(this.scroll);
+
+  }
 
   ngOnInit(): void {
 
@@ -69,76 +75,91 @@ export class NavMenuComponent implements OnInit {
       case AppRoutingConstants.masksState.stateHome:
         this.workflowRoutingService.goTo(AppRoutingConstants.masksState.stateHome);
         this.closeNav();
+        this.scrollToTopComponent.scrollToTop();
         break;
 
       case AppRoutingConstants.masksState.stateWhoIAm:
-        this.closeNav();
         this.workflowRoutingService.goTo(AppRoutingConstants.masksState.stateWhoIAm);
+        this.closeNav();
+        this.scrollToTopComponent.scrollToTop();
         break;
 
       case AppRoutingConstants.masksState.stateTellAboutMe:
         this.workflowRoutingService.goTo(AppRoutingConstants.masksState.stateTellAboutMe);
         this.closeNav();
+        this.scrollToTopComponent.scrollToTop();
         break;
 
       case AppRoutingConstants.masksState.stateArmocromia:
         this.workflowRoutingService.goTo(AppRoutingConstants.masksState.stateArmocromia);
         this.closeNav();
+        this.scrollToTopComponent.scrollToTop();
         break;
 
       case AppRoutingConstants.masksState.stateWedding:
         this.workflowRoutingService.goTo(AppRoutingConstants.masksState.stateWedding);
         this.closeNav();
+        this.scrollToTopComponent.scrollToTop();
         break;
 
       case AppRoutingConstants.masksState.stateLookChange:
         this.workflowRoutingService.goTo(AppRoutingConstants.masksState.stateLookChange);
         this.closeNav();
+        this.scrollToTopComponent.scrollToTop();
         break;
 
       case AppRoutingConstants.masksState.stateFaceShapes:
         this.workflowRoutingService.goTo(AppRoutingConstants.masksState.stateFaceShapes);
         this.closeNav();
+        this.scrollToTopComponent.scrollToTop();
         break;
 
       case AppRoutingConstants.masksState.stateBodyShapes:
         this.workflowRoutingService.goTo(AppRoutingConstants.masksState.stateBodyShapes);
         this.closeNav();
+        this.scrollToTopComponent.scrollToTop();
         break;
 
       case AppRoutingConstants.masksState.stateMakeUpInPalette:
         this.workflowRoutingService.goTo(AppRoutingConstants.masksState.stateMakeUpInPalette);
         this.closeNav();
+        this.scrollToTopComponent.scrollToTop();
         break;
 
       case AppRoutingConstants.masksState.stateBlogArticles:
         this.workflowRoutingService.goTo(AppRoutingConstants.masksState.stateBlogArticles);
         this.closeNav();
+        this.scrollToTopComponent.scrollToTop();
         break;
 
       case AppRoutingConstants.masksState.stateBlogVideos:
         this.workflowRoutingService.goTo(AppRoutingConstants.masksState.stateBlogVideos);
         this.closeNav();
+        this.scrollToTopComponent.scrollToTop();
         break;
 
       case AppRoutingConstants.masksState.stateBlogPhotos:
         this.workflowRoutingService.goTo(AppRoutingConstants.masksState.stateBlogPhotos);
         this.closeNav();
+        this.scrollToTopComponent.scrollToTop();
         break;
 
       case AppRoutingConstants.masksState.stateQualifications:
         this.workflowRoutingService.goTo(AppRoutingConstants.masksState.stateQualifications);
         this.closeNav();
+        this.scrollToTopComponent.scrollToTop();
         break;
 
       case AppRoutingConstants.masksState.stateServicesList:
         this.workflowRoutingService.goTo(AppRoutingConstants.masksState.stateServicesList);
         this.closeNav();
+        this.scrollToTopComponent.scrollToTop();
         break;
 
       case AppRoutingConstants.masksState.stateContacts:
         this.workflowRoutingService.goTo(AppRoutingConstants.masksState.stateContacts);
         this.closeNav();
+        this.scrollToTopComponent.scrollToTop();
         break;
 
     }
