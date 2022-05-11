e8b0764c0336ff79198edf2f25cbc87115165eab
Simone Crisafi <simone.crisafi@nttdata.com>
2022-05-11 17:56:24 +0200

    update blog photos component

---
+++ src/app/components/blog/services/blog.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {AngularFireStorage} from "@angular/fire/compat/storage";
+import {Photo, PhotoFactory} from "../../../data-model/classes/Photo";
+import {environment} from "../../../../environments/environment";
+import {db} from "../../../../main";
+import { query, where, orderBy, limit } from "firebase/firestore";
+import { addDoc, collection, getDocs } from "firebase/firestore";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BlogService {
+
+  constructor(private afStorage: AngularFireStorage) { }
+
+  /** Servizio che recupera le 3 foto più 'nuove' inserite nel blog **/
+  public async getPhotos(): Promise<Photo[]> {
+
+    let photoList: Photo[] = [];
+    // const getImagesQuery = query(collection(db, "/blog/media/immagini"), where("population", ">", 100000), orderBy("population"), limit(2));
+    const getImagesQuery = query(collection(db, environment.endpoint.blog.immagini));
+    const querySnapshot = await getDocs(getImagesQuery);
+    querySnapshot.forEach((doc) => {
+      let photo = PhotoFactory.getInstanceFromObject(doc.data());
+      /** Recupera l'url dallo storage da usare nell'HTML*/
+      photo.imagePathFromStorage = this.afStorage.ref(photo.imagePath).getDownloadURL();
+      photoList.push(photo);
+    });
+    return photoList;
+
+  }
+
+}
