38e401ae3273a01012c1cc80487a6da85b3acab0
Simone Crisafi <simone.crisafi@nttdata.com>
2022-06-10 10:51:53 +0200

    add services list navigation

--- src/app/components/services-list/services-list.component.ts
+++ src/app/components/services-list/services-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ScrollToTopComponent} from "../shared/components/scroll-to-top/scroll-to-top.component";
+import {WorkflowRoutingService} from "../shared/services/workflow-routing.service";
+import {ViewportScroller} from "@angular/common";
+import {AppRoutingConstants} from "../../app-routingConstants";
 
 @Component({
   selector: 'app-services-list',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicesListComponent implements OnInit {
 
-  constructor() { }
+  scrollToTopComponent: ScrollToTopComponent;
 
-  ngOnInit(): void {
+  constructor(private workflowRoutingService: WorkflowRoutingService,
+              private scroll: ViewportScroller) {
+
+    this.scrollToTopComponent = new ScrollToTopComponent(this.scroll);
+
+  }
+
+  ngOnInit(): void {}
+
+  /** Gestione navigazione */
+  public goTo(forwardState: string) {
+
+    switch (forwardState) {
+
+      case AppRoutingConstants.masksState.stateServicesListStyleAnalysis:
+        this.workflowRoutingService.goTo(AppRoutingConstants.masksState.stateServicesListStyleAnalysis);
+        this.scrollToTopComponent.scrollToTop();
+        break;
+
+      case AppRoutingConstants.masksState.stateServicesListShopping:
+        this.workflowRoutingService.goTo(AppRoutingConstants.masksState.stateServicesListShopping);
+        this.scrollToTopComponent.scrollToTop();
+        break;
+
+      case AppRoutingConstants.masksState.stateServicesListImageConsulting:
+        this.workflowRoutingService.goTo(AppRoutingConstants.masksState.stateServicesListImageConsulting);
+        this.scrollToTopComponent.scrollToTop();
+        break;
+
+    }
+
   }
 
 }
