c788f90faf3bd816057935cdae16d8b3c86fd895
Simone Crisafi <simone.crisafi@nttdata.com>
2022-06-27 11:53:29 +0200

    add works navigation

--- src/app/components/works/components/works/works.component.ts
+++ src/app/components/works/components/works/works.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ScrollToTopComponent} from "../../../shared/components/scroll-to-top/scroll-to-top.component";
+import {WorkflowRoutingService} from "../../../shared/services/workflow-routing.service";
+import {ViewportScroller} from "@angular/common";
+import {AppRoutingConstants} from "../../../../app-routingConstants";
 
 @Component({
   selector: 'app-works',
@@ -7,9 +11,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorksComponent implements OnInit {
 
-  constructor() { }
+  scrollToTopComponent: ScrollToTopComponent;
 
-  ngOnInit(): void {
+  constructor(private workflowRoutingService: WorkflowRoutingService,
+              private scroll: ViewportScroller) {
+
+    this.scrollToTopComponent = new ScrollToTopComponent(this.scroll);
+
+  }
+
+  ngOnInit(): void {}
+
+  /** Gestione navigazione */
+  public goTo(forwardState: string) {
+
+    switch (forwardState) {
+
+      case AppRoutingConstants.masksState.stateArmocromia:
+        this.workflowRoutingService.goTo(AppRoutingConstants.masksState.stateArmocromia);
+        this.scrollToTopComponent.scrollToTop();
+        break;
+
+      case AppRoutingConstants.masksState.stateWedding:
+        this.workflowRoutingService.goTo(AppRoutingConstants.masksState.stateWedding);
+        this.scrollToTopComponent.scrollToTop();
+        break;
+
+      case AppRoutingConstants.masksState.stateLookChange:
+        this.workflowRoutingService.goTo(AppRoutingConstants.masksState.stateLookChange);
+        this.scrollToTopComponent.scrollToTop();
+        break;
+
+      case AppRoutingConstants.masksState.stateFaceShapes:
+        this.workflowRoutingService.goTo(AppRoutingConstants.masksState.stateFaceShapes);
+        this.scrollToTopComponent.scrollToTop();
+        break;
+
+      case AppRoutingConstants.masksState.stateBodyShapes:
+        this.workflowRoutingService.goTo(AppRoutingConstants.masksState.stateBodyShapes);
+        this.scrollToTopComponent.scrollToTop();
+        break;
+
+    }
+
   }
 
 }
