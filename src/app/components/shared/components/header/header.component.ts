3467a1ecf54228be42f8dfa681ca679f0efe9f69
Simone Crisafi <simone.crisafi@nttdata.com>
2022-06-27 16:22:32 +0200

    update headerComponent

--- src/app/components/shared/components/header/header.component.ts
+++ src/app/components/shared/components/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ScrollToTopComponent} from "../scroll-to-top/scroll-to-top.component";
+import {WorkflowRoutingService} from "../../services/workflow-routing.service";
+import {ViewportScroller} from "@angular/common";
+import {AppRoutingConstants} from "../../../../app-routingConstants";
 
 @Component({
   selector: 'app-header',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  scrollToTopComponent: ScrollToTopComponent;
+
+  constructor(private workflowRoutingService: WorkflowRoutingService,
+              private scroll: ViewportScroller) {
+
+    this.scrollToTopComponent = new ScrollToTopComponent(this.scroll);
+
+  }
 
   ngOnInit(): void {
+  }
+
+  public goToHome(): void {
+    this.workflowRoutingService.goTo(AppRoutingConstants.masksState.stateHome);
+    this.scrollToTopComponent.scrollToTop();
   }
 
 }
